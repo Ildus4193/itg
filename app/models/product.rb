04dd044b546242582b64df117d6e030666d0812e
Ildus <sadykov1988@gmail.com>
2011-11-27 22:03:04 +0400

    catalog is modified

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "954x889>", :thumb => "233x207>" }
-  belongs_to :catalog_mebelis
+  belongs_to :catalog_mebeli
 end
